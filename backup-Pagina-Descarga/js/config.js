5ab4fc3ecf65c308714f16a45a5d6ef88279e991
Sara Denis <sara.gutierrez@salesup.com.mx>
2016-09-24 16:07:01 -0500

    Second carpet with files of templates for download page

---
+++ backup-Pagina-Descarga/js/config.js
@@ -0,0 +1,28 @@
+var config={
+   consulta_registro:'http://control.salesup.com.mx/webservices/ValidaProspecto.dbsp',
+  /* ingresa_registro:'https://socrates.salesup.com.mx/guarda_prospecto.dbsp',*/
+
+   tke:'E-62B83098-11FC-4CD3-AA4A-E93DE1C72D6B',
+   id_empresa:'11811',
+   sp_version:'2',
+   /*id_usuario:'33946',
+   id_origen_prospecto:'2850',
+   id_etiqueta_registro:'3510',
+   id_etiqueta_asistencia:'3511',
+   id_etiqueta_registro_sistema:'3592',
+   id_etiqueta_registro_experto:'3593',
+   id_etiqueta_asistencia_sistema:'3594',
+   id_etiqueta_asistencia_experto:'3595'
+   */
+
+   ingresa_registro:'http://dev.salesup.com.mx/guarda_prospecto.dbsp',//desarrollo
+   id_usuario:'56',//desarrollo
+   id_origen_prospecto:'3051',//desarrollo
+   id_etiqueta_registro:'2814',//desarrollo
+   id_etiqueta_asistencia:'2815',//desarrollo
+   id_etiqueta_registro_experto:'2816',//desarrollo
+   id_etiqueta_asistencia_experto:'2818',//desarrollo
+   id_etiqueta_registro_sistema:'2817',//desarrollo
+   id_etiqueta_asistencia_sistema:'2819'//desarrollo
+   
+};
---
+++ backup-Pagina-Descarga/js/validaciones.js
@@ -0,0 +1,83 @@
+function despintar(elemento){elemento.style.borderColor = "#ccc";elemento.style.color = "#000000";elemento.style.backgroundColor = "#fff";}
+function validar() {
+	var nombre = $('#nombre').val();
+	var correo = $('#correo').val();
+	var compania = $('#compania').val();
+	var puesto = $('#puesto').val();
+	var telefono = $('#telefono').val();
+	var industria = $('#industria').val();
+
+	var emailreg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    var telreg=/^\d{10}$/;
+
+	if (nombre!=""&& correo!=""&&emailreg.test(correo)&&compania!=""&&puesto!=""&&telefono!="" && telreg.test(telefono)&&industria!="")
+	{
+		//alert('hecho');
+		var form={
+			nombre:nombre,
+			correo:correo,
+			telefono:telefono,
+			compania:compania,
+			puesto:puesto,
+			industria:industria
+		};
+		//console.log(form);
+		enviar(form);
+		return false;
+	}
+	else
+	{
+		if (nombre==""){$('#nombre').css('border-color','red');}
+		if (correo==""|| !emailreg.test(correo)){$('#correo').css('border-color','red');}
+		if (compania==""){$('#compania').css('border-color','red');}
+		if (puesto==""){$('#puesto').css('border-color','red');}
+		if (telefono==""|| !telreg.test(telefono)){$('#telefono').css('border-color','red');}
+		if (industria==""){$('#industria').css('border-color','red');}
+		return false;
+	}
+	return false;
+}
+
+function enviar(datos)
+{
+	//console.log(datos);
+    var form=
+    {
+        IDEMPRESA:config.id_empresa,
+        NOMBRE:datos.nombre,
+        TELEFONO:datos.telefono,
+        COMPANIA:datos.compania,
+        CORREO:datos.correo,
+        PUESTO:datos.puesto,
+        INDUSTRIA:datos.industria,
+        IDORIGEN:config.id_origen_prospecto,
+        IDETIQUETA:config.id_etiqueta_registro,
+        IDUSUARIO:config.id_usuario,
+        COMENTARIOS:"descarga",
+        SP_VERSION:2
+    };
+    //console.log(form);
+    var request= $.ajax(
+    {
+      method: "POST",
+      url: config.ingresa_registro,
+      data: form,
+    })
+    request.done(function(res)
+    {
+        alert("Correo Enviado");
+        //console.log(res);
+        //alert( "success:" + res);
+        
+    })
+    request.fail(function(jqXHR, textStatus)
+    {
+        console.log("____________");
+        console.log(jqXHR);
+        console.log("____________");
+        console.log(textStatus);
+        console.log("____________");
+        alert("Error de envio!");
+        //alert( "error -" + jqXHR +"-" + textStatus);
+    })
+}
