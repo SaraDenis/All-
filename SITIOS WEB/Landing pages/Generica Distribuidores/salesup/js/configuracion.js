46ce0d268866e51e4a0365c41a03130e8f329574
Sara Denis <sara.gutierrez@salesup.com.mx>
2016-09-24 21:52:34 -0500

    C

---
+++ SITIOS WEB/Landing pages/Generica Distribuidores/salesup/js/configuracion.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+/*  Colocar el ID del distribuidor */
+SalesUp.Sistema.Almacenamiento({a:'Distribuidor', v:'121'});
+
+SalesUp.Variables.deDonde = 'LandingDistribuidor';
+SalesUp.Variables.noGracias = true;
+
+SalesUp.Variables.pEmail = '';
+SalesUp.Variables.pContrasenia = '';
+SalesUp.Variables.pRepContrasenia = '';
+SalesUp.Variables.formaSimple = '';
+
+SalesUp.Demo.PruebaloSignUp();
+
+   $('a[href*="pruebalo"], .linkPruebalo').click(function(e){
+  e.preventDefault();
+  pos = $('#Nombre_SingUp').offset().top-120;
+     $("html, body").animate({
+scrollTop: pos}, 1000);
+ $('#Nombre_SingUp').focus();
+ return false;
+   });
+});
