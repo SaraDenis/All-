142487b8346cf16c42ae774f2bc47e2dfd83ccd2
Sara Denis <sara.gutierrez@salesup.com.mx>
2016-09-24 20:14:01 -0500

    Seventh carpet with files of exmples of landing download

---
+++ process/js/config-para-pruebas.js
@@ -0,0 +1,37 @@
+   var config={
+   consulta_registro:'http://control.salesup.com.mx/webservices/ValidaProspecto.dbsp',
+   ingresa_registro:'https://socrates.salesup.com.mx/guarda_prospecto.dbsp',
+   tke:'E-62B83098-11FC-4CD3-AA4A-E93DE1C72D6B',
+   id_empresa:'11811', //No quitar
+   sp_version:'2', //No quitar
+   id_usuario:'64211',//ID de Sarahi
+   id_origen_prospecto:'3071',
+   id_etiqueta_registro:'3091',
+
+   //Para hacer pruebas-------
+   consulta_registro:'http://control.salesup.com.mx/webservices/ValidaProspecto.dbsp',
+   ingresa_registro:'http://dev.salesup.com.mx/guarda_prospecto.dbsp',//desarrollo
+   tke:'E-62B83098-11FC-4CD3-AA4A-E93DE1C72D6B',
+   id_empresa:'11811', //No quitar
+   sp_version:'2', //No quitar
+   id_usuario:'56',//desarrollo
+   id_origen_prospecto:'3051',//desarrollo
+   id_etiqueta_registro:'2814',//desarrollo
+
+   //
+   /*
+   id_origen_prospecto:'2850', //Lo tengo que consultar
+   id_etiqueta_registro:'3510', //solicitar el id de las etiquetas que se utilizaran y cambiarlos en validaciones.js
+   id_etiqueta_asistencia:'3511',
+   id_etiqueta_registro_sistema:'3592',
+   id_etiqueta_registro_experto:'3593',
+   id_etiqueta_asistencia_sistema:'3594',
+   id_etiqueta_asistencia_experto:'3595'
+
+   id_etiqueta_asistencia:'2815',//desarrollo
+   id_etiqueta_registro_experto:'2816',//desarrollo
+   id_etiqueta_asistencia_experto:'2818',//desarrollo
+   id_etiqueta_registro_sistema:'2817',//desarrollo
+   id_etiqueta_asistencia_sistema:'2819'//desarrollo
+   */
+}
---
+++ process/js/config.js
@@ -0,0 +1,10 @@
+var config={
+   consulta_registro:'http://control.salesup.com.mx/webservices/ValidaProspecto.dbsp',
+   ingresa_registro:'https://socrates.salesup.com.mx/guarda_prospecto.dbsp',
+   tke:'E-62B83098-11FC-4CD3-AA4A-E93DE1C72D6B', //no quitar
+   id_empresa:'11811', //No quitar
+   sp_version:'2', //No quitar
+   id_usuario:'64211',//ID de Sarahi
+   id_origen_prospecto:'3071',//web Ebook
+   id_etiqueta_registro:'3091'//mql
+};
