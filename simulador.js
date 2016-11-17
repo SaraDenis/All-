adced02999e60504068aa3f70c2477be502c939f
saradenis <sara.gutierrez@salesup.com.mx>
2016-11-17 17:37:08 -0500

    simulador changes

--- simulador.js
+++ simulador.js
@@ -1,3 +1,14 @@
+function showSimulator(){
+	$("#botonS").click(function(){
+		$("#datosSU").show();
+		$("#resultSU").show();
+		$("#comparison").show();
+		$("#gain").show();
+		$("#charts").show();
+	});
+	ConSalesup();
+
+}
 function SinSalesup() {
 	var day = document.getElementById("day").value;
 	var time = document.getElementById("time").value;
@@ -13,15 +24,6 @@ function SinSalesup() {
 	var result6=document.getElementById("Result6");
 	var result7=document.getElementById("Result7");
 
-	var form={
-		day:day.value,
-		time:time.value,
-		quotes:quotes.value,
-		hours:hours.value,
-		sales:sales.value,
-		salary:salary.value
-	};
-
 	result1 = quotes/day;
 	jQuery("#Result1").val(result1.toFixed(1));
 	result2 = result1*time;
@@ -36,14 +38,13 @@ function SinSalesup() {
 	jQuery("#Result6").val(result6.toFixed(1));
 	result7 = (time*quotes*sales)/60/8;
 	jQuery("#Result7").val(result7.toFixed(1));
-}
-function ConSalesup(){
-	var daySU = document.getElementById("daySU").value;
-	var timeSU = document.getElementById("timeSU").value;
-	var quotesSU = document.getElementById("quotesSU").value;
+
+	//var daySU = document.getElementById("daySU").value;
+	var timeSU = 2;
+	/*var quotesSU = document.getElementById("quotesSU").value;
 	var hoursSU = document.getElementById("hoursSU").value;
 	var salesSU = document.getElementById("salesSU").value;
-	var salarySU = document.getElementById("salarySU").value;
+	var salarySU = document.getElementById("salarySU").value;*/
 	var result1SU=document.getElementById("Result1SU");
 	var result2SU=document.getElementById("Result2SU");
 	var result3SU=document.getElementById("Result3SU");
@@ -52,29 +53,53 @@ function ConSalesup(){
 	var result6SU=document.getElementById("Result6SU");
 	var result7SU=document.getElementById("Result7SU");
 
-	result1SU = quotesSU/daySU;
+	result1SU = result1;
 	jQuery("#Result1SU").val(result1SU.toFixed(1));
 	result2SU = result1SU*timeSU;
 	jQuery("#Result2SU").val(result2SU.toFixed(1));
-	result3SU = (result2SU/(hoursSU*60))*100;
+	result3SU = (result2SU/(hours*60))*100;
 	jQuery("#Result3SU").val(result3SU.toFixed(0)+"%");
-	result4SU = (timeSU*quotesSU*salesSU)/60;
+	result4SU = (timeSU*quotes*sales)/60;
 	jQuery("#Result4SU").val(result4SU.toFixed(1));
-	result5SU = (salarySU/daySU/hoursSU/60)*timeSU;
+	result5SU = (salary/day/hours/60)*timeSU;
 	jQuery("#Result5SU").val(result5SU.toFixed(1));
-	result6SU = result5SU*quotesSU*salesSU;
-	jQuery("#Result6SU").val(result6SU.toFixed(1));
-	result7SU = (timeSU*quotesSU*salesSU)/60/8;
+	result6SU = result5SU*quotes*sales;
+	jQuery("#Result6SU").val(result6SU.toFixed());
+	result7SU = (timeSU*quotes*sales)/60/8;
 	jQuery("#Result7SU").val(result7SU.toFixed(1));
-}
 
+/* comparaciones */
+	jQuery("#actual1").val(result2.toFixed(1));
+	jQuery("#actual2").val(result4.toFixed(1));
+	jQuery("#actual3").val(result6.toFixed(1));
+	jQuery("#actual4").val(result7.toFixed(1));
+	var reActual5 = quotes*time/60/hours;
+	jQuery("#actual5").val(reActual5.toFixed(1));
+
+	jQuery("#conSU1").val(result2SU.toFixed(1));
+	jQuery("#conSU2").val(result4SU.toFixed(1));
+	jQuery("#conSU3").val(result6SU.toFixed());
+	jQuery("#conSU4").val(result7SU.toFixed(1));
+	var reConSU5 = quotes*timeSU/60/hours;
+	jQuery("#conSU5").val(reConSU5.toFixed(1));
+
+	var conSU1 = result2SU;
+	var actual1 = result2;
+	var Cvar1= conSU1/actual1;
+	jQuery("#var1").val(Cvar1.toFixed()+"%");
+	jQuery("#var2").val();
+	jQuery("#var3").val();
+	jQuery("#var4").val();
+	jQuery("#var5").val();
+}
+/*
 function Comparaciones(){
-	var actual1 = document.getElementById("actual1");
+	
 	var actual2 = document.getElementById("actual2");
 	var actual3 = document.getElementById("actual3");
 	var actual4 = document.getElementById("actual4");
 	var actual5 = document.getElementById("actual5");
-	var conSU1 = document.getElementById("conSU1");
+	
 	var conSU2 = document.getElementById("conSU2");
 	var conSU3 = document.getElementById("conSU3");
 	var conSU4 = document.getElementById("conSU4");
@@ -87,3 +112,4 @@ function Comparaciones(){
 	
 	jQuery("#actual1").val(result2);
 }
+*/
