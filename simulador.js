f3096455b9ed5ee53349721118657d0cc6476e34
saradenis <sara.gutierrez@salesup.com.mx>
2016-11-18 17:43:02 -0500

    Simulador changes 18

--- simulador.js
+++ simulador.js
@@ -6,8 +6,6 @@ function showSimulator(){
 		$("#gain").show();
 		$("#charts").show();
 	});
-	ConSalesup();
-
 }
 function SinSalesup() {
 	var day = document.getElementById("day").value;
@@ -39,12 +37,7 @@ function SinSalesup() {
 	result7 = (time*quotes*sales)/60/8;
 	jQuery("#Result7").val(result7.toFixed(1));
 
-	//var daySU = document.getElementById("daySU").value;
 	var timeSU = 2;
-	/*var quotesSU = document.getElementById("quotesSU").value;
-	var hoursSU = document.getElementById("hoursSU").value;
-	var salesSU = document.getElementById("salesSU").value;
-	var salarySU = document.getElementById("salarySU").value;*/
 	var result1SU=document.getElementById("Result1SU");
 	var result2SU=document.getElementById("Result2SU");
 	var result3SU=document.getElementById("Result3SU");
@@ -83,33 +76,43 @@ function SinSalesup() {
 	var reConSU5 = quotes*timeSU/60/hours;
 	jQuery("#conSU5").val(reConSU5.toFixed(1));
 
-	var conSU1 = result2SU;
-	var actual1 = result2;
-	var Cvar1= conSU1/actual1;
+
+	var comparison1 = document.getElementById("Result2").value;
+	var comparison2 = document.getElementById("Result2SU").value;
+	var Cvar1 = (comparison2/comparison1-1)*100;
+
+	var comparison3 = document.getElementById("Result4").value;
+	var comparison4 = document.getElementById("Result4SU").value;
+	var Cvar2= (comparison4/comparison3-1)*100;
+
+	var comparison5 = document.getElementById("Result6").value;
+	var comparison6 = document.getElementById("Result6SU").value;
+	var Cvar3 = comparison6-comparison5;
+
+	var comparison7 = document.getElementById("Result7").value;
+	var comparison8 = document.getElementById("Result7SU").value;
+	var Cvar4 = comparison7-comparison8;
+
+	var comparison9 = document.getElementById("actual5").value;
+	var comparison10 = document.getElementById("conSU5").value;
+	var Cvar5 = comparison9-comparison10;
+
 	jQuery("#var1").val(Cvar1.toFixed()+"%");
-	jQuery("#var2").val();
-	jQuery("#var3").val();
-	jQuery("#var4").val();
-	jQuery("#var5").val();
+	jQuery("#var2").val(Cvar2.toFixed()+"%");
+	jQuery("#var3").val(Cvar3.toFixed(2));
+	jQuery("#var4").val(Cvar4.toFixed());
+	jQuery("#var5").val(Cvar5.toFixed(1));
+
+	jQuery("#gain1").val(result6.toFixed(1));
+	jQuery("#gain2").val(result6SU.toFixed());
+	var Rgain1 = salary*sales;
+	jQuery("#Tgain1").val(Rgain1.toFixed(1));
+	
+	
+	var Tsalary = Rgain1-result6; 
+	jQuery("#salary1").val(Tsalary.toFixed(2));
+	jQuery("#Tgain2").val(Rgain1.toFixed(1));
+	var Tsalary2 = Rgain1-result6SU; 
+	jQuery("#salary2").val(Tsalary2.toFixed(2));	
 }
-/*
-function Comparaciones(){
-	
-	var actual2 = document.getElementById("actual2");
-	var actual3 = document.getElementById("actual3");
-	var actual4 = document.getElementById("actual4");
-	var actual5 = document.getElementById("actual5");
-	
-	var conSU2 = document.getElementById("conSU2");
-	var conSU3 = document.getElementById("conSU3");
-	var conSU4 = document.getElementById("conSU4");
-	var conSU5 = document.getElementById("conSU5");
-	var var1 = document.getElementById("var1");
-	var var2 = document.getElementById("var2");
-	var var3 = document.getElementById("var3");
-	var var4 = document.getElementById("var4");
-	var var5 = document.getElementById("var5");
-	
-	jQuery("#actual1").val(result2);
-}
-*/
+
