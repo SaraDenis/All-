d12f66e7b636a0652df4923d92c67a11e2e682e9
saradenis <sara.gutierrez@salesup.com.mx>
2017-02-17 18:11:45 -0500

    para el pop up de salesup

--- process/popup.js
+++ process/popup.js
@@ -1,9 +1,17 @@
-$(document).on('mouseleave',function(){
- 	if (event.pageY<=1) {
- 		$(".modalP").fadeIn();
- 	}
-    $(".cerrarP").click(function(){
-     
-        $(".modalP").fadeOut(300);
-    });
+$("html").ready(function(){
+	function ValidaEsCorreo(v){
+      var re = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      return re.test(v);
+    }
+});
+$( document ).on( "mouseleave", function() {
+	if (localStorage.getItem("tiempo")===null) { 
+		if (event.pageY<=1) {
+		$(".modalP").fadeIn();
+		}
+		$(".cerrarP").click(function(){
+		localStorage.setItem('tiempo', 1); 
+		$(".modalP").fadeOut(300);
+		});
+	}
 });
